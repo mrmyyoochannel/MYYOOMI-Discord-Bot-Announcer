4e01fdf633e385a433351a40cee0e634659d51bc
MYYOOMI <93776430+mrmyyoochannel@users.noreply.github.com>
2025-07-24 23:42:20 +0700

    Update Install requirements.cmd

--- Install requirements.cmd
+++ Install requirements.cmd
@@ -1,6 +1,12 @@
+echo off
+echo Start install Requirements.
+title install Requirements.
+
 pip install Flask
 pip install discord.py
 pip install python-dotenv
 pip install watchdog
 pip install flask-cors
+
+echo End install Requirements.
 pluse
